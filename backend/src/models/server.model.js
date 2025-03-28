61f4052e5fa3a9f14eabd58a678fa69d22f0a2fd
Rakesh <rakesh.31.3@mountblue.tech>
2025-03-28 15:16:07 +0530

    Feat:implemented the models for the project

---
+++ backend/src/models/server.model.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema, model } from "mongoose";
+import MongooseDelete from "mongoose-delete";
+const serverSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    icon: {
+      type: String,
+      default: "",
+    },
+    owner: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    members: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    channels: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Channel",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+serverSchema.plugin(MongooseDelete, {
+  deletedAt: true,
+  overrideMethods: "all",
+});
+const Server = model("Server", serverSchema);
+export default Server;
